1f656babe9a9504c7b828a3c0e3a197d7c5c3143
Garrett Luu <garrettluu131@gmail.com>
2019-08-05 18:07:19 -0700

    First incorrect for LengthOfLength

--- LengthOfLongestSubstring/Solution.java
+++ LengthOfLongestSubstring/Solution.java
@@ -1,14 +1,18 @@
 class Solution {
   public int lengthOfLongestSubstring(String s) {
     String current = "";
-    String longest = "";
+    String longest = new String();
     for (int i = 0; i < s.length(); i++) {
       if (!current.contains("" + s.charAt(i))) {
         current += s.charAt(i);
       } else {
         current = "" + s.charAt(i);
       }
+
+      if (current.length() > longest.length()) {
+        longest = new String(current);
+      }
     }
-    return current.length();
+    return longest.length();
   }
 }
