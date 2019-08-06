eacba7ea5055ae69f238c05db10324d2e4a7eab5
Garrett Luu <garrettluu131@gmail.com>
2019-08-05 18:35:32 -0700

    Added outer loop

--- LengthOfLongestSubstring/Solution.java
+++ LengthOfLongestSubstring/Solution.java
@@ -2,15 +2,19 @@ class Solution {
   public int lengthOfLongestSubstring(String s) {
     String current = "";
     String longest = new String();
-    for (int i = 0; i < s.length(); i++) {
-      if (!current.contains("" + s.charAt(i))) {
-        current += s.charAt(i);
-      } else {
-        current = "" + s.charAt(i);
-      }
 
-      if (current.length() > longest.length()) {
-        longest = new String(current);
+    for (int i = 0; i < s.length(); i++) {
+      current = "" + s.charAt(i);
+      for (int j = i + 1; j < s.length(); j++) {
+        if (!current.contains("" + s.charAt(j))) {
+          current += s.charAt(j);
+        } else {
+          current = "" + s.charAt(j);
+        }
+
+        if (current.length() > longest.length()) {
+          longest = new String(current);
+        }
       }
     }
     return longest.length();
