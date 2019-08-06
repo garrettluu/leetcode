d05056909dbce2299462f33cd4c5a01d594cb7a9
Garrett Luu <garrettluu131@gmail.com>
2019-08-06 13:52:54 -0700

    minor fix

--- LongestPalindrome/Solution.java
+++ LongestPalindrome/Solution.java
@@ -4,7 +4,7 @@ class Solution {
     //Naive solution:
     //Check all possible substrings for palindromes
     for (int i = 0; i < s.length(); i++) {
-      for (int j = i + 1; j < s.length(); j++) {
+      for (int j = i + 1; j <= s.length(); j++) {
         String sub = s.substring(i, j);
         if (isPalindrome(sub)) {
           if (sub.length() > longest.length()) {
