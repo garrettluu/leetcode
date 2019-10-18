046e94c3670810ec91cbdb13ae93f010281009f8
Garrett Luu <garrettluu131@gmail.com>
2019-10-17 21:02:08 -0700

    Attempt at O(n^2) solution

--- 3Sum/Solution.java
+++ 3Sum/Solution.java
@@ -1,32 +1,34 @@
-import java.util.*;
-
 class Solution {
   public List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
-    //Brute force solution?
-    for (int i = 0; i < nums.length - 2; i++) {
-      for (int j = i + 1; j < nums.length - 1; j++) {
-        for (int k = j + 1; k < nums.length; k++) {
-          if (nums[i] + nums[j] + nums[k] == 0) {
-            List<Integer> triplet = new ArrayList<>();
-            triplet.add(nums[i]);
-            triplet.add(nums[j]);
-            triplet.add(nums[k]);
-            if (!tripletIsInList(triplet, result)) {
-              result.add(triplet);
-            }
-          }
+    int n = nums.length;
+    for (int i = 0; i < n - 1; i++) {
+      int left = i + 1;
+      int right = n - 1;
+      while (left < right) {
+        if (nums[i] == -(nums[left] + nums[right])) {
+          List<Integer> triplet = new ArrayList<>();
+          triplet.add(nums[i]);
+          triplet.add(nums[left]);
+          triplet.add(nums[right]);
+          result.add(triplet);
         }
+        left++;
+      }
+
+      left = i + 1;
+      right = i + 2;
+      while (right < n - 1) {
+        if (nums[i] == -(nums[left] + nums[right])) {
+          List<Integer> triplet = new ArrayList<>();
+          triplet.add(nums[i]);
+          triplet.add(nums[left]);
+          triplet.add(nums[right]);
+          result.add(triplet);
+        }
+        right++;
       }
     }
-
     return result;
-  }
-
-  // Check if a triplet is already in the list
-  private boolean tripletIsInList(List<Integer> triplet,
-      List<List<Integer>> list) {
-    Collections.sort(triplet);
-    return list.contains(triplet);
   }
 }
