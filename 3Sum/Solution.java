ae849c0ecff5e69e43ffc40cbef6bb5f753d24d6
Garrett Luu <garrettluu131@gmail.com>
2019-10-17 18:41:29 -0700

    Fixed issue of picking same number multiple times

--- 3Sum/Solution.java
+++ 3Sum/Solution.java
@@ -2,10 +2,9 @@ class Solution {
   public List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
     //Brute force solution?
-    //Need a way to avoid duplicates
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i; j < nums.length; j++) {
-        for (int k = j; k < nums.length; k++) {
+    for (int i = 0; i < nums.length - 2; i++) {
+      for (int j = i + 1; j < nums.length - 1; j++) {
+        for (int k = j + 1; k < nums.length; k++) {
           if (nums[i] + nums[j] + nums[k] == 0) {
             List<Integer> triplet = new ArrayList<>();
             triplet.add(nums[i]);
