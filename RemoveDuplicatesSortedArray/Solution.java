77e07fa3415f88fc4c6b7527378be349c5097d41
Garrett Luu <garrettluu131@gmail.com>
2019-12-24 14:48:13 -0800

    Finished RemoveDuplicates

--- RemoveDuplicatesSortedArray/Solution.java
+++ RemoveDuplicatesSortedArray/Solution.java
@@ -1,18 +1,28 @@
 class Solution {
   public int removeDuplicates(int[] nums) {
-    int origLength = nums.length;
-    int deleted = 0;
-    for (int i = 0; i < nums.length - 1; i++) {
+    int length = nums.length;
+    for (int i = 0; i < length - 1; i++) {
+      int deletedNow = 0;
       //If we found a duplicate
       if (nums[i] == nums[i+1]) {
         int j = i + 1;
         //Keep going until we stop finding duplicates
-        while (nums[j] == nums[j-1]) {
+        while (j < length && nums[j] == nums[j-1]) {
           j++;
+          deletedNow++;
         }
-        
-        
+
+        //Move elements over
+        int l = 1;
+        for (int k = j; k < length; k++) {
+          nums[i+l] = nums[k];
+          l++;
+        }
+
+        //Update length of array
+        length -= deletedNow;
       }
     }
+    return length;
   }
 }
